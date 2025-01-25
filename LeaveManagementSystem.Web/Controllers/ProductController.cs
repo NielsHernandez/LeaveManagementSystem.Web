6f46fbf3aee740a998ee8b820cfd198a1b5551a7
niels <niels@DESKTOP-NCJCCJV>
2025-01-25 16:10:26 -0600

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LeaveManagementSystem.Web.Models;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            //bind data to the view
+
+            var data = new Product
+            {
+                Name = "Fruits Cake",
+                Quantity = 10
+
+            };
+
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagementSystem.Web/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementSystem.Web.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
